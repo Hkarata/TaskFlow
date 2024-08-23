3f34e11f1094fbcb309d5bee9e4def66e03244b3
Heri Karata <herishine00@gmail.com>
2024-08-23 22:09:24 +0300

    Refactor EditItemViewModel and update EditItemPage
    Refactored EditItemViewModel to use CommunityToolkit.Mvvm for observable properties and commands. Updated EditItemPage.xaml to bind UI elements to the view model, enhancing MVVM compliance. Cleaned up unused using directives in related files.

--- TaskFlow/ViewModels/EditItemViewModel.cs
+++ TaskFlow/ViewModels/EditItemViewModel.cs
@@ -1,12 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using TaskFlow.Models;
+using TaskFlow.Repositories;
 
 namespace TaskFlow.ViewModels
 {
     public partial class EditItemViewModel : BaseViewModel
     {
+        [ObservableProperty]
+        private TodoItem? _todoItem;
+
+        [ObservableProperty]
+        private ComboBoxItem? _selectedPriority;
+
+        [ObservableProperty]
+        private ComboBoxItem? _selectedFlag;
+
+        private readonly ITodoRepository? _todoRepository;
+
+        public EditItemViewModel()
+        {
+            var mainWindow = Application.Current.MainWindow as MainWindow;
+            mainWindow!.MainFrame.Navigated += MainFrame_Navigated;
+            _todoRepository = App.ServiceProvider!.GetRequiredService<ITodoRepository>();
+        }
+
+        private void MainFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+            if (e.ExtraData is TodoItem item)
+            {
+                TodoItem = item;
+                SelectedPriority = new ComboBoxItem();
+                SelectedFlag = new ComboBoxItem();
+                SelectedPriority!.Content = item.Priority.ToString();
+                SelectedFlag!.Content = item.Flag.ToString();
+            }
+        }
+
+        [RelayCommand]
+        private void GoBack()
+        {
+            NavigationService.GoBack();
+        }
+
+        [RelayCommand]
+        private async Task Save()
+        {
+            var todoItem = new TodoItem
+            {
+                Id = TodoItem!.Id,
+                Description = TodoItem!.Description,
+                Priority = (ItemPriority)Enum.Parse(typeof(ItemPriority), SelectedPriority!.Content.ToString()!),
+                Flag = (ItemFlag)Enum.Parse(typeof(ItemFlag), SelectedFlag!.Content.ToString()!),
+                TodoList = TodoItem!.TodoList
+            };
+
+            await _todoRepository!.CreateOrUpdateToDo(todoItem);
+
+            NavigationService.NavigateTo("ListDetailsPage", todoItem.TodoList);
+        }
     }
 }
